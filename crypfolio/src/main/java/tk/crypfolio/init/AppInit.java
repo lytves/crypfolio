532440d233aca149de56251390da6dcd5ab2f367
lytves <lytves@gmail.com>
2018-04-15 21:13:38 +0200

    application start servlet implemented

---
+++ crypfolio/src/main/java/tk/crypfolio/init/AppInit.java
@@ -0,0 +1,54 @@
+package tk.crypfolio.init;
+
+import tk.crypfolio.DAO.AbstractDAOFactory;
+import tk.crypfolio.DAO.CoinDAO;
+import tk.crypfolio.common.SettingsDB;
+import tk.crypfolio.domain.CoinEntity;
+import tk.crypfolio.parse.ParserAPI;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AppInit implements ServletContextListener {
+
+    private static final Logger logger = Logger.getLogger(AppInit.class.getName());
+
+    /*
+    * application startup method
+    * */
+    public void contextInitialized(ServletContextEvent sce) {
+
+        logger.log(Level.INFO, "CrypFolio application initializing.");
+
+        initTableCoins();
+
+    }
+
+
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+
+    /*
+    * Insert all parsed coin to SettingsDB.APP_DB_TYPE - DataBase
+    * */
+    private void initTableCoins(){
+
+        ParserAPI parserAPI = new ParserAPI();
+        ArrayList<CoinEntity> listCoins = (ArrayList<CoinEntity>) parserAPI.parseAllCoin();
+
+        AbstractDAOFactory myFactory = AbstractDAOFactory.getDAOFactory(SettingsDB.APP_DB_TYPE);
+
+        CoinDAO cDAO = myFactory.getCoinDAO();
+
+        for (CoinEntity coin: listCoins){
+
+            cDAO.createCoin(coin);
+        }
+
+    }
+
+}
