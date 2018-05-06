36c10e3c741bedd549c4b5982485fd28e8a346aa
lytves <lytves@gmail.com>
2018-05-06 06:16:08 +0200

    initial web resources

---
+++ crypfolio/web/resources/js/default.js
@@ -0,0 +1,8 @@
+/* it's a redicule use case:
+ when rendered bean method execute link action not works,
+  cause I am using JavaScript to click it*/
+/*
+function invokeCommandLink() {
+    var jsfCommandLink = document.getElementById("resendPanel");
+    jsfCommandLink.click();
+}*/
