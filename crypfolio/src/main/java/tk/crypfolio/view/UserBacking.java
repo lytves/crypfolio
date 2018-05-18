14cbe2c2508aceebf554da46af5bfdc3d8b2bcaf
lytves <lytves@gmail.com>
2018-05-19 00:21:40 +0200

    UserBackingBean

---
+++ crypfolio/src/main/java/tk/crypfolio/view/UserBacking.java
@@ -0,0 +1,41 @@
+package tk.crypfolio.view;
+
+import tk.crypfolio.business.ApplicationContainer;
+import tk.crypfolio.business.UserService;
+import tk.crypfolio.model.UserEntity;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class UserBacking implements Serializable {
+
+    // session scoped
+    @Inject
+    private ActiveUser activeUser;
+
+    // application scoped
+    @Inject
+    private ApplicationContainer applicationContainer;
+
+    // stateless business
+    @Inject
+    private UserService userService;
+
+    private UserEntity user;
+
+    @PostConstruct
+    private void init(){
+
+        this.user = activeUser.getUser();
+
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+}
