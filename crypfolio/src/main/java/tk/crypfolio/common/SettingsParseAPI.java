0f79e1f220b7ca086b99c5a59f2cbc5c3b331b06
lytves <lytves@gmail.com>
2018-10-05 20:46:13 +0200

    CMC API coin by ticker added

--- crypfolio/src/main/java/tk/crypfolio/common/SettingsParseAPI.java
+++ crypfolio/src/main/java/tk/crypfolio/common/SettingsParseAPI.java
@@ -2,5 +2,7 @@ package tk.crypfolio.common;
 
 public abstract class SettingsParseAPI {
 
-    public static final String ALL_COINS_LIST = "https://api.coinmarketcap.com/v2/listings/";
+    public static final String CMC_ALL_COINS_LIST = "https://api.coinmarketcap.com/v2/listings/";
+
+    public static final String CMC_COIN_BY_TICKER_ID = "https://api.coinmarketcap.com/v2/ticker/";
 }
