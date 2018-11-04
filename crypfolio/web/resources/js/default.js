475dcc5fb9586f8f4ba63e4f2e51549c618329c3
lytves <lytves@gmail.com>
2018-11-04 21:46:34 +0100

    whatchlist and account-menu changes, portfolio && archive tabs created

--- crypfolio/web/resources/js/default.js
+++ crypfolio/web/resources/js/default.js
@@ -1,8 +1,34 @@
 function addWatchCoin() {
-    document.getElementById('divAddCoin').focus();
+    document.getElementById('divWatchAddCoin').focus();
 
-    coinInput = document.getElementById('mainTabView:formAddCoin:add-coin-name_input');
-    document.getElementById('mainTabView:formAddCoin:add-coin-name_input').setAttribute("placeholder", coinInput.value);
+    coinInput = document.getElementById('mainTabView:formWatchAddCoin:add-coin-name_input');
+    document.getElementById('mainTabView:formWatchAddCoin:add-coin-name_input').setAttribute("placeholder", coinInput.value);
 
     coinInput.value = '';
+}
+
+/**
+ * Listener to trigger modal close, when clicked on dialog mask
+ * got it from https://stackoverflow.com/questions/14595835/primefaces-dialog-close-on-click-outside-of-the-dialog
+ */
+$(document).ready(function(){
+    $("body").on("click",'.ui-dialog-mask',function () {
+        idModal = this.id;
+        idModal = idModal.replace("_modal","");
+        getWidgetVarById(idModal).hide();
+    })
+});
+
+/**
+ * Returns the PrimefacesWidget from ID
+ * @param id
+ * @returns {*}
+ */
+function getWidgetVarById(id) {
+    for (var propertyName in PrimeFaces.widgets) {
+        var widget = PrimeFaces.widgets[propertyName];
+        if (widget && widget.id === id) {
+            return widget;
+        }
+    }
 }
