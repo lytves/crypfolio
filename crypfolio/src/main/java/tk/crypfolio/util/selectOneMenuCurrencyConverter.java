c1aa2f44189abf4e5c1ee97cb9205000344c197f
lytves <lytves@gmail.com>
2018-11-12 20:29:40 +0100

    selectOneMenuCurrencyConverter for p:selectOneMenu select position currency type

---
+++ crypfolio/src/main/java/tk/crypfolio/util/selectOneMenuCurrencyConverter.java
@@ -0,0 +1,50 @@
+package tk.crypfolio.util;
+
+import tk.crypfolio.common.CurrencyType;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.inject.Named;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A utility class-converter, is using to convert CurrencyType object to String to show it in the dialog window
+ */
+@Named
+public class selectOneMenuCurrencyConverter implements Converter {
+
+    private static final Logger logger = Logger.getLogger(selectOneMenuCurrencyConverter.class.getName());
+
+    @Override
+    public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String value) {
+
+        if (value != null && value.trim().length() > 0) {
+
+            try {
+
+                for (CurrencyType currency : CurrencyType.values()) {
+
+                    if (currency.getCurrency().equals(String.valueOf(value))) {
+                        return currency;
+                    }
+                }
+
+            } catch (NumberFormatException e) {
+                logger.log(Level.WARNING, "Error selectOneMenuCurrencyConverter getAsObject!");
+            }
+
+        }
+        return null;
+    }
+
+    @Override
+    public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object object) {
+        if (object != null) {
+            return String.valueOf(((CurrencyType) object).getCurrency());
+        } else {
+            return null;
+        }
+    }
+}
