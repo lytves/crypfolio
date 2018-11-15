f43ebc02899087f4c29600ca60fb8871e200bb38
lytves <lytves@gmail.com>
2018-11-15 20:48:39 +0100

    selectOneButtonPositionTypeConverter for p:selectOneButton select position currency type

--- crypfolio/src/main/java/tk/crypfolio/util/SelectOneButtonPositionTypeConverter.java
+++ crypfolio/src/main/java/tk/crypfolio/util/SelectOneButtonPositionTypeConverter.java
@@ -1,4 +1,50 @@
 package tk.crypfolio.util;
 
-public class SelectOneButtonPositionTypeConverter {
+import tk.crypfolio.common.PositionType;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.inject.Named;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * A utility class-converter, is using to convert PositionType object to String to show it in the dialog window
+ */
+@Named
+public class SelectOneButtonPositionTypeConverter implements Converter {
+
+    private static final Logger logger = Logger.getLogger(SelectOneButtonPositionTypeConverter.class.getName());
+
+    @Override
+    public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String value) {
+
+        if (value != null && value.trim().length() > 0) {
+
+            try {
+
+                for (PositionType type : PositionType.values()) {
+
+                    if (type.getType().equals(String.valueOf(value))) {
+                        return type;
+                    }
+                }
+
+            } catch (NumberFormatException e) {
+                logger.log(Level.WARNING, "Error SelectOneButtonPositionTypeConverter getAsObject!");
+            }
+
+        }
+        return null;
+    }
+
+    @Override
+    public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object object) {
+
+        if (object != null) {
+            return (String) object;
+        }
+        return null;
+    }
 }
