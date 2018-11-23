1074a68fde08db43773d7c46c82084a2441c9677
lytves <lytves@gmail.com>
2018-11-23 16:30:09 +0100

    bought cost renamed to net cost && isArchived parameter added, addTransaction(TransactionEntity transaction) in development

--- crypfolio/src/main/java/tk/crypfolio/model/ItemEntity.java
+++ crypfolio/src/main/java/tk/crypfolio/model/ItemEntity.java
@@ -1,6 +1,7 @@
 package tk.crypfolio.model;
 
 import tk.crypfolio.common.CurrencyType;
+import tk.crypfolio.common.TransactionType;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -15,37 +16,40 @@ public class ItemEntity implements Serializable {
 
     @Id
     @Column(name = "item_id", nullable = false)
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "item_amount", nullable = false, precision = 8)
     private BigDecimal amount = BigDecimal.ZERO;
 
     @Enumerated(EnumType.STRING)
-    @Column(name="item_showed_currency", nullable = false)
+    @Column(name = "item_showed_currency", nullable = false)
     private CurrencyType showedCurrency = CurrencyType.USD;
 
     @Basic
-    @Column(name = "item_bought_cost_usd", precision = 8)
-    private BigDecimal boughtCostUsd = BigDecimal.ZERO;
+    @Column(name = "item_net_cost_usd", precision = 8)
+    private BigDecimal netCostUsd = BigDecimal.ZERO;
 
-    @Column(name = "item_bought_cost_eur", precision = 8)
-    private BigDecimal boughtCostEur = BigDecimal.ZERO;
+    @Column(name = "item_net_cost_eur", precision = 8)
+    private BigDecimal netCostEur = BigDecimal.ZERO;
 
-    @Column(name = "item_bought_cost_btc", precision = 8)
-    private BigDecimal boughtCostBtc = BigDecimal.ZERO;
+    @Column(name = "item_net_cost_btc", precision = 8)
+    private BigDecimal netCostBtc = BigDecimal.ZERO;
 
-    @Column(name = "item_bought_cost_eth", precision = 8)
-    private BigDecimal boughtCostEth = BigDecimal.ZERO;
+    @Column(name = "item_net_cost_eth", precision = 8)
+    private BigDecimal netCostEth = BigDecimal.ZERO;
 
-//    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @Column(name = "item_is_archived", nullable = false)
+    private Boolean isArchived = false;
+
+    //    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @ManyToOne
-    @JoinColumn(name="portfolios_users_us_id", referencedColumnName="users_us_id", nullable = false)
+    @JoinColumn(name = "portfolios_users_us_id", referencedColumnName = "users_us_id", nullable = false)
     private PortfolioEntity portfolio;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
+    //    @ManyToOne(fetch = FetchType.LAZY)
     @ManyToOne
-    @JoinColumn(name = "coins_coin_id", referencedColumnName="coin_id", nullable = false)
+    @JoinColumn(name = "coins_coin_id", referencedColumnName = "coin_id", nullable = false)
     private CoinEntity coin;
 
     @OneToMany(mappedBy = "item", cascade = CascadeType.ALL)
@@ -59,16 +63,24 @@ public class ItemEntity implements Serializable {
         this.coin = coin;
     }
 
-    public void addTransaction(TransactionEntity transaction){
+    public void addTransaction(TransactionEntity transaction) {
         this.transactions.add(transaction);
         // setting also for new transaction this item-parent
         transaction.setItem(this);
 
-        this.amount = this.amount.add(transaction.getAmount());
-        this.boughtCostUsd = this.boughtCostUsd.add(transaction.getBoughtPriceUsd());
-        this.boughtCostEur = this.boughtCostEur.add(transaction.getBoughtPriceEur());
-        this.boughtCostBtc = this.boughtCostBtc.add(transaction.getBoughtPriceBtc());
-        this.boughtCostEth = this.boughtCostEth.add(transaction.getBoughtPriceEth());
+        if (transaction.getType().equals(TransactionType.BUY)) {
+            this.amount = this.amount.add(transaction.getAmount());
+
+        } else if (transaction.getType().equals(TransactionType.SELL)) {
+            // if is SELL transaction and we should increase the amount,
+            // but we should never have negative final amount, so return positive or ZERO
+            this.amount = this.amount.subtract(transaction.getAmount()).max(BigDecimal.ZERO);
+        }
+
+        this.netCostUsd = this.netCostUsd.add(transaction.getBoughtPriceUsd());
+        this.netCostEur = this.netCostEur.add(transaction.getBoughtPriceEur());
+        this.netCostBtc = this.netCostBtc.add(transaction.getBoughtPriceBtc());
+        this.netCostEth = this.netCostEth.add(transaction.getBoughtPriceEth());
     }
 
     public Long getId() {
@@ -95,36 +107,44 @@ public class ItemEntity implements Serializable {
         this.showedCurrency = showedCurrency;
     }
 
-    public BigDecimal getBoughtCostUsd() {
-        return boughtCostUsd;
+    public BigDecimal getNetCostUsd() {
+        return netCostUsd;
     }
 
-    public void setBoughtCostUsd(BigDecimal boughtCostUsd) {
-        this.boughtCostUsd = boughtCostUsd;
+    public void setNetCostUsd(BigDecimal netCostUsd) {
+        this.netCostUsd = netCostUsd;
     }
 
-    public BigDecimal getBoughtCostEur() {
-        return boughtCostEur;
+    public BigDecimal getNetCostEur() {
+        return netCostEur;
     }
 
-    public void setBoughtCostEur(BigDecimal boughtCostEur) {
-        this.boughtCostEur = boughtCostEur;
+    public void setNetCostEur(BigDecimal netCostEur) {
+        this.netCostEur = netCostEur;
     }
 
-    public BigDecimal getBoughtCostBtc() {
-        return boughtCostBtc;
+    public BigDecimal getNetCostBtc() {
+        return netCostBtc;
     }
 
-    public void setBoughtCostBtc(BigDecimal boughtCostBtc) {
-        this.boughtCostBtc = boughtCostBtc;
+    public void setNetCostBtc(BigDecimal netCostBtc) {
+        this.netCostBtc = netCostBtc;
     }
 
-    public BigDecimal getBoughtCostEth() {
-        return boughtCostEth;
+    public BigDecimal getNetCostEth() {
+        return netCostEth;
     }
 
-    public void setBoughtCostEth(BigDecimal boughtCostEth) {
-        this.boughtCostEth = boughtCostEth;
+    public void setNetCostEth(BigDecimal netCostEth) {
+        this.netCostEth = netCostEth;
+    }
+
+    public Boolean getArchived() {
+        return isArchived;
+    }
+
+    public void setArchived(Boolean archived) {
+        isArchived = archived;
     }
 
     public PortfolioEntity getPortfolio() {
@@ -159,10 +179,10 @@ public class ItemEntity implements Serializable {
         return Objects.equals(getId(), that.getId()) &&
                 Objects.equals(getAmount(), that.getAmount()) &&
                 getShowedCurrency() == that.getShowedCurrency() &&
-                Objects.equals(getBoughtCostUsd(), that.getBoughtCostUsd()) &&
-                Objects.equals(getBoughtCostEur(), that.getBoughtCostEur()) &&
-                Objects.equals(getBoughtCostBtc(), that.getBoughtCostBtc()) &&
-                Objects.equals(getBoughtCostEth(), that.getBoughtCostEth()) &&
+                Objects.equals(getNetCostUsd(), that.getNetCostUsd()) &&
+                Objects.equals(getNetCostEur(), that.getNetCostEur()) &&
+                Objects.equals(getNetCostBtc(), that.getNetCostBtc()) &&
+                Objects.equals(getNetCostEth(), that.getNetCostEth()) &&
                 Objects.equals(getPortfolio(), that.getPortfolio()) &&
                 Objects.equals(getCoin(), that.getCoin()) &&
                 Objects.equals(getTransactions(), that.getTransactions());
@@ -171,8 +191,8 @@ public class ItemEntity implements Serializable {
     @Override
     public int hashCode() {
 
-        return Objects.hash(getId(), getAmount(), getShowedCurrency(), getBoughtCostUsd(), getBoughtCostEur(),
-                getBoughtCostBtc(), getBoughtCostEth(), getPortfolio(), getCoin(), getTransactions());
+        return Objects.hash(getId(), getAmount(), getShowedCurrency(), getNetCostUsd(), getNetCostEur(),
+                getNetCostBtc(), getNetCostEth(), getPortfolio(), getCoin(), getTransactions());
     }
 
     @Override
@@ -181,10 +201,10 @@ public class ItemEntity implements Serializable {
                 "id=" + id +
                 ", amount=" + amount +
                 ", showedCurrency=" + showedCurrency +
-                ", boughtCostUsd=" + boughtCostUsd +
-                ", boughtCostEur=" + boughtCostEur +
-                ", boughtCostBtc=" + boughtCostBtc +
-                ", boughtCostEth=" + boughtCostEth +
+                ", netCostUsd=" + netCostUsd +
+                ", netCostEur=" + netCostEur +
+                ", netCostBtc=" + netCostBtc +
+                ", netCostEth=" + netCostEth +
                 ", coin=" + coin +
                 ", transactions=" + transactions +
                 '}';
