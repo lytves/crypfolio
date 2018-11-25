3783b2f50d0c014cca04b192ae2ea4eabb78f713
lytves <lytves@gmail.com>
2018-11-25 15:45:10 +0100

    BigDecimal MATH_CONTEXT_8_PRECISION to set - scale and round parameters

--- crypfolio/src/main/java/tk/crypfolio/common/Settings.java
+++ crypfolio/src/main/java/tk/crypfolio/common/Settings.java
@@ -1,5 +1,8 @@
 package tk.crypfolio.common;
 
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 public abstract class Settings {
 
     public static final Integer EMAIL_VERIFICATION_LINK_TIMELIFE_SECONDS = 1800;
@@ -9,5 +12,7 @@ public abstract class Settings {
     public static final String ADMIN_EMAIL = "PUT_YOUR_GMAIL_ID_HERE";
 
     public static final String ADMIN_EMAIL_PASSWORD = "PUT_YOUR_GMAIL_PASSWORD_HERE";
+
+    public static final MathContext MATH_CONTEXT_8_PRECISION = new MathContext(8, RoundingMode.HALF_UP);
 
 }
