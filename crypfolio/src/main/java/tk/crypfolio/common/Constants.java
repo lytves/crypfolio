1b6e8d31b1688d334ce8355cf5ce21fa496836ee
lytves <lytves@gmail.com>
2018-12-12 00:58:53 +0100

    new dateShortPattern constant to use it in datatable with list of transactions in the item modal window

--- crypfolio/src/main/java/tk/crypfolio/common/Constants.java
+++ crypfolio/src/main/java/tk/crypfolio/common/Constants.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 public abstract class Constants {
 
     public static final String datePattern = "dd MMMM, yyyy";
+    public static final String dateShortPattern = "dd MMM, yyyy";
     public static final Locale mainLocale = Locale.ENGLISH;
 
 }
