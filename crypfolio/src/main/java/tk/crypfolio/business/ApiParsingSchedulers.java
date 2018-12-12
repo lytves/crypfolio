d7be61df17bdc84be2d5e0583867290bf35920b2
lytves <lytves@gmail.com>
2018-12-12 18:35:25 +0100

    so after previous commit we can set API parsing scheduler to 30min, more less suitable value to use in production with real Professional CMC API

--- crypfolio/src/main/java/tk/crypfolio/business/ApiParsingSchedulers.java
+++ crypfolio/src/main/java/tk/crypfolio/business/ApiParsingSchedulers.java
@@ -46,7 +46,7 @@ public class ApiParsingSchedulers {
 
 //    https://docs.oracle.com/javaee/6/tutorial/doc/bnboy.html - Using the Timer Service
 
-    @Schedule(minute = "*/5", hour = "*", persistent = false)
+    @Schedule(minute = "*/30", hour = "*", persistent = false)
     @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
     public void parseAllCoinsOnSandboxCMC() {
 
