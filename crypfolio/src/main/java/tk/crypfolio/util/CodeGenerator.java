d9ca55574849484b4e5e668a31fc3b3978aa8900
lytves <lytves@gmail.com>
2018-12-19 19:51:27 +0100

    javadoc to class

--- crypfolio/src/main/java/tk/crypfolio/util/CodeGenerator.java
+++ crypfolio/src/main/java/tk/crypfolio/util/CodeGenerator.java
@@ -2,6 +2,9 @@ package tk.crypfolio.util;
 
 import java.util.UUID;
 
+/**
+ * A utility class to generate UUID codes to send for user in cases of email verification or reset password
+ */
 public abstract class CodeGenerator {
 
     public static String generateCodeUUID(){
