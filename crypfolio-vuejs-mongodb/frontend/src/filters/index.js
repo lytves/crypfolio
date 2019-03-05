554ddf1bd7063e162dba5b004b86ad1ff932195b
lytves <22356770+lytves@users.noreply.github.com>
2019-03-05 23:37:58 +0100

    vuejs-mongodb: added a general filter for all values, depends of the currency

--- crypfolio-vuejs-mongodb/frontend/src/filters/index.js
+++ crypfolio-vuejs-mongodb/frontend/src/filters/index.js
@@ -22,3 +22,32 @@ Vue.filter('portfolioValues', function (value, currency) {
 
     return formatter.format(value);
 });
+
+Vue.filter('generalValues', function (value, currency) {
+
+    if (typeof value !== "number") {
+        return value;
+    }
+
+    let maximumFractionDigits = 2;
+
+    switch (currency) {
+        case 'USD':
+        case 'EUR':
+            if (value < 1) {
+                maximumFractionDigits = 4;
+            }
+            break;
+        // increase the number of fraction digits
+        case 'BTC':
+        case 'ETH':
+            maximumFractionDigits = 8;
+            break;
+    }
+
+    let formatter = new Intl.NumberFormat('en-US', {
+        maximumFractionDigits: maximumFractionDigits,
+    });
+
+    return formatter.format(value);
+});
