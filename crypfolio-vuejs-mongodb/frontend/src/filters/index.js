145b66de3efbe89e93c64231eb03f7aa269f22d9
lytves <22356770+lytves@users.noreply.github.com>
2019-03-06 15:10:56 +0100

    vuejs-mongodb: added filters for percents and marketcap values

--- crypfolio-vuejs-mongodb/frontend/src/filters/index.js
+++ crypfolio-vuejs-mongodb/frontend/src/filters/index.js
@@ -51,3 +51,47 @@ Vue.filter('generalValues', function (value, currency) {
 
     return formatter.format(value);
 });
+
+Vue.filter('percentsValues', function (value) {
+
+    if (typeof value !== "number") {
+        return value;
+    }
+
+    let formatter = new Intl.NumberFormat('en-US', {
+        maximumFractionDigits: 2,
+    });
+
+    return formatter.format(value);
+});
+
+
+Vue.filter('marketcapValues', function (num, locale = 'en') {
+
+    if (typeof num !== "number") {
+        return num;
+    }
+
+    let formatter = new Intl.NumberFormat('en-US', {
+        maximumFractionDigits: 2,
+    });
+
+
+    // Nine Zeroes for Billions
+    return Math.abs(Number(num)) >= 1.0e+9
+
+        ? formatter.format(Math.abs(Number(num)) / 1.0e+9) + "B"
+
+        // Six Zeroes for Millions
+        : Math.abs(Number(num)) >= 1.0e+6
+
+            ? formatter.format(Math.abs(Number(num)) / 1.0e+6) + "M"
+
+            // Three Zeroes for Thousands
+            : Math.abs(Number(num)) >= 1.0e+3
+
+                ? formatter.format(Math.abs(Number(num) / 1.0e+3)) + "k"
+
+                : formatter.format(Math.abs(Number(num)));
+
+});
