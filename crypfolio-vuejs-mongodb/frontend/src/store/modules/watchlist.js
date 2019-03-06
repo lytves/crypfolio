9bb8106c515aad8b6fb075efe37b7dd2eddba674
lytves <22356770+lytves@users.noreply.github.com>
2019-03-06 15:14:15 +0100

    vuejs-mongodb: implemented Vuex action WATCHLIST_CHANGE_COIN_CURRENCY

--- crypfolio-vuejs-mongodb/frontend/src/store/modules/watchlist.js
+++ crypfolio-vuejs-mongodb/frontend/src/store/modules/watchlist.js
@@ -1,5 +1,5 @@
-import {WATCHLIST_SUCCESS, WATCHLIST_ERROR} from '../actions/watchlist'
-import {SNACKBAR_ERROR, SNACKBAR_SUCCESS} from "../actions/snackbar";
+import {WATCHLIST_CHANGE_COIN_CURRENCY, WATCHLIST_ERROR, WATCHLIST_SUCCESS} from '../actions/watchlist'
+import {SNACKBAR_ERROR} from "../actions/snackbar";
 import {userWatchlistService} from "../../utils";
 
 const state = {
@@ -12,7 +12,22 @@ const getters = {
 
 const actions = {
     [WATCHLIST_SUCCESS]: ({commit}, watchlist) => {
-        commit(WATCHLIST_SUCCESS, watchlist)
+        commit(WATCHLIST_SUCCESS, watchlist);
+    },
+    [WATCHLIST_CHANGE_COIN_CURRENCY]: ({commit, dispatch}, {coinId, currency}) => {
+        return new Promise((resolve, reject) => {
+
+            return userWatchlistService.setCoinShowedCurrency(coinId, currency)
+                .then(resp => {
+
+                    commit(WATCHLIST_CHANGE_COIN_CURRENCY, {coinId, currency});
+
+                })
+                .catch(err => {
+                    dispatch(SNACKBAR_ERROR, "Error on changing coin's currency!");
+                    reject(err)
+                })
+        })
     },
 };
 
@@ -20,13 +35,19 @@ const mutations = {
     [WATCHLIST_SUCCESS]: (state, watchlist) => {
         state.userWatchlist = watchlist;
         console.log('watchlist!!!', state.userWatchlist);
-        if (!!state.userWatchlist)
-            console.log('true');
-        else
-            console.log('false');
     },
-    [WATCHLIST_ERROR]: (state) => {
-        state.userWatchlist = {};
+    [WATCHLIST_CHANGE_COIN_CURRENCY]: (state, {coinId, currency}) => {
+
+        state.userWatchlist.find(v => v.coinId.id === coinId).showedCurrency = currency;
+
+        /* let ku = state.userWatchlist.find(function(e) {
+            return e.coinId.id === coinId;
+        });
+        ku.showedCurrency = currency;*/
+
+    },
+    [WATCHLIST_ERROR]: (state, message) => {
+        // state.userWatchlist = {};
     }
 };
 export default {
@@ -35,3 +56,45 @@ export default {
     actions,
     mutations,
 }
+
+/*    [WATCHLIST_REQUESSSSSSSSSSST]: ({commit}, watchlist) => {
+        commit(WATCHLIST_SUCCESS, watchlist);
+
+        let timerId = setTimeout(function tick() {
+
+            console.log('tick');
+
+            userPortfolioService.getApplicationContainer()
+                .then(resp => {
+
+                    console.log(resp);
+/!*                    // parsing of response to have User entity as JSON
+                    let user = JSON.parse(resp);
+
+                    // save object Portfolio to separated 'store portfolio'
+                    dispatch(PORTFOLIO_SUCCESS, user.portfolio);
+
+                    // save array userWatchCoins to separated 'store watchlist'
+                    dispatch(WATCHLIST_SUCCESS, user.userWatchCoins);
+
+                    commit(USER_SUCCESS, user);
+
+                    // and also store new auth token to auth.store.sate
+                    commit(AUTH_SUCCESS);*!/
+
+                })
+                .catch(err => {
+                    // commit(USER_ERROR, err);
+                    // if resp is unauthorized, logout
+                    // dispatch(AUTH_LOGOUT)
+                });
+
+            timerId = setTimeout(tick, 60000);
+        }, 0);
+
+
+
+        /!*        setInterval(() => {
+            console.log('fuck intervaaaaaaaaaaaaaaaaal');
+        }, 5000)*!/
+    },*/
