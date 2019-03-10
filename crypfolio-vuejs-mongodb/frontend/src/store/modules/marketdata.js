e3c71f2024c8c92aa781d7390c4755b07fb5e52f
lytves <22356770+lytves@users.noreply.github.com>
2019-03-10 21:43:44 +0100

    vuejs-mongodb: Vuex store module 'marketdata', added  MARKETDATA_ADDCOIN_TO_USERCOINS

--- crypfolio-vuejs-mongodb/frontend/src/store/modules/marketdata.js
+++ crypfolio-vuejs-mongodb/frontend/src/store/modules/marketdata.js
@@ -1,14 +1,20 @@
-import {MARKETDATA_ALLCOINSLIST_SUCCESS, MARKETDATA_ERROR, MARKETDATA_USERCOINS_SUCCESS} from '../actions/marketdata'
+import {
+    MARKETDATA_ADDCOIN_TO_USERCOINS,
+    MARKETDATA_ALLCOINSLIST_SUCCESS,
+    MARKETDATA_ERROR,
+    MARKETDATA_USERCOINS_SUCCESS
+} from '../actions/marketdata'
 import {marketdataService} from "../../utils";
 import {SNACKBAR_ERROR} from "../actions/snackbar";
 
 const state = {
-    userCoinsMarketData: [],
+    userCoinsMarketData: {},
     allCoinsListData: [],
 };
 
 const getters = {
     isUserCoinsMarketDataLoaded: state => !!state.userCoinsMarketData,
+    isAllCoinsListDataLoaded: state => (Array.isArray(state.allCoinsListData) && state.allCoinsListData.length > 0),
 };
 
 const actions = {
@@ -22,6 +28,24 @@ const actions = {
                     let userCoinsMarketData = JSON.parse(resp);
 
                     commit(MARKETDATA_USERCOINS_SUCCESS, userCoinsMarketData)
+                })
+                .catch(err => {
+                    dispatch(SNACKBAR_ERROR, "Error on receiving actual market data!");
+                    reject(err)
+                })
+        })
+    },
+    [MARKETDATA_ADDCOIN_TO_USERCOINS]: ({commit, dispatch}, coinId) => {
+        return new Promise((resolve, reject) => {
+
+            return marketdataService.getCoinData(coinId)
+                .then(resp => {
+
+                    // parsing of response to have a map like a JSON
+                    let coinMarketData = JSON.parse(resp);
+
+                    commit(MARKETDATA_ADDCOIN_TO_USERCOINS, coinMarketData);
+                    resolve(resp)
                 })
                 .catch(err => {
                     dispatch(SNACKBAR_ERROR, "Error on receiving actual market data!");
@@ -55,12 +79,15 @@ const mutations = {
     [MARKETDATA_USERCOINS_SUCCESS]: (state, userCoinsMarketData) => {
 
         state.userCoinsMarketData = userCoinsMarketData;
-        console.log('userCoinsMarketData!!!', state.userCoinsMarketData);
+
+    },
+    [MARKETDATA_ADDCOIN_TO_USERCOINS]: (state, coinMarketData) => {
+
+        state.userCoinsMarketData = Object.assign(state.userCoinsMarketData, coinMarketData);
     },
     [MARKETDATA_ALLCOINSLIST_SUCCESS]: (state, allCoinsListData) => {
 
         state.allCoinsListData = allCoinsListData;
-        console.log('allCoinsListData!!!', state.allCoinsListData);
     },
     [MARKETDATA_ERROR]: (state) => {
         // state.userWatchlist = {};
@@ -73,3 +100,45 @@ export default {
     actions,
     mutations,
 }
+
+/*    [WATCHLIST_REQUESSSSSSSSSSST]: ({commit}, watchlist) => {
+        commit(WATCHLIST_SUCCESS, watchlist);
+
+        let timerId = setTimeout(function tick() {
+
+            console.log('tick');
+
+            userPortfolioService.getApplicationContainer()
+                .then(resp => {
+
+                    console.log(resp);
+/!*                    // parsing of response to have User entity as JSON
+                    let user = JSON.parse(resp);
+
+                    // save object Portfolio to separated 'store portfolio'
+                    dispatch(PORTFOLIO_SUCCESS, user.portfolio);
+
+                    // save array userWatchCoins to separated 'store watchlist'
+                    dispatch(WATCHLIST_SUCCESS, user.userWatchCoins);
+
+                    commit(USER_SUCCESS, user);
+
+                    // and also store new auth token to auth.store.sate
+                    commit(AUTH_SUCCESS);*!/
+
+                })
+                .catch(err => {
+                    // commit(USER_ERROR, err);
+                    // if resp is unauthorized, logout
+                    // dispatch(AUTH_LOGOUT)
+                });
+
+            timerId = setTimeout(tick, 60000);
+        }, 0);
+
+
+
+        /!*        setInterval(() => {
+            console.log('intervaaaaaaaaaaaaaaaaal');
+        }, 5000)*!/
+    },*/
