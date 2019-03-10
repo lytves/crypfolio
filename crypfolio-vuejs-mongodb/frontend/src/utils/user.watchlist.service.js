12dc8225e5d194a5c412132e5762938a174fee30
lytves <22356770+lytves@users.noreply.github.com>
2019-03-10 21:56:37 +0100

    vuejs-mongodb: 'user.watchlist' implemented endpoint service "/watchlist-add-new-coin"

--- crypfolio-vuejs-mongodb/frontend/src/utils/user.watchlist.service.js
+++ crypfolio-vuejs-mongodb/frontend/src/utils/user.watchlist.service.js
@@ -2,6 +2,7 @@ import AXIOS from './axios-instance';
 
 export const userWatchlistService = {
     setCoinShowedCurrency,
+    addNewWatchlistCoin
 };
 
 async function setCoinShowedCurrency(coinId, currency) {
@@ -14,6 +15,22 @@ async function setCoinShowedCurrency(coinId, currency) {
                 // we don't need to pass any data,
                 // coz it's a request only for set new user's watch coin currency
                 // if response was success, so set Coin ShowedCurrency has been done successfully
+            })
+        .catch(error => {
+            return Promise.reject(error.response);
+        });
+}
+
+async function addNewWatchlistCoin(coinId, currency) {
+    return await AXIOS.put('/watchlist-add-new-coin', {
+        "coinId": coinId,
+        "currency": currency,
+    })
+        .then(
+            response => {
+                // will return the status of the request to get know if the adding was done successfully
+                // or there was an error and the coin is already in the watchlist
+                return response.data.status;
             })
         .catch(error => {
             return Promise.reject(error.response);
