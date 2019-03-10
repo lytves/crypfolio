317f15194060fa7b1857409c2e12f797f63ccef9
lytves <22356770+lytves@users.noreply.github.com>
2019-03-10 21:51:39 +0100

    vuejs-mongodb: 'marketdata' implemented endpoint service "/coin-data/coinId"

--- crypfolio-vuejs-mongodb/frontend/src/utils/marketdata.service.js
+++ crypfolio-vuejs-mongodb/frontend/src/utils/marketdata.service.js
@@ -2,6 +2,7 @@ import AXIOS from './axios-instance';
 
 export const marketdataService = {
     getUserCoinsData,
+    getCoinData,
     getAllCoinsListData
 };
 
@@ -10,6 +11,18 @@ async function getUserCoinsData(allUserCoinsIds) {
         .then(
             response => {
                 // returns only JSON object with coins actual market data grouped by coin's IDs
+                return response.data.data;
+            })
+        .catch(error => {
+            return Promise.reject(error.response);
+        });
+}
+
+async function getCoinData(coinId) {
+    return await AXIOS.get(`/coin-data/` + coinId)
+        .then(
+            response => {
+                // returns only JSON object with a coin actual market data
                 return response.data.data;
             })
         .catch(error => {
