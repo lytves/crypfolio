a1ed2a19ce5da850b415cda572202dbb3d332f04
lytves <22356770+lytves@users.noreply.github.com>
2019-03-10 21:49:31 +0100

    vuejs-mongodb: Vuex watchlist added WATCHLIST_ADD_NEW_COIN

--- crypfolio-vuejs-mongodb/frontend/src/store/modules/watchlist.js
+++ crypfolio-vuejs-mongodb/frontend/src/store/modules/watchlist.js
@@ -1,6 +1,12 @@
-import {WATCHLIST_CHANGE_COIN_CURRENCY, WATCHLIST_ERROR, WATCHLIST_SUCCESS} from '../actions/watchlist'
-import {SNACKBAR_ERROR} from "../actions/snackbar";
+import {
+    WATCHLIST_ADD_NEW_COIN,
+    WATCHLIST_CHANGE_COIN_CURRENCY,
+    WATCHLIST_ERROR,
+    WATCHLIST_SUCCESS
+} from '../actions/watchlist'
+import {SNACKBAR_ERROR, SNACKBAR_SUCCESS} from "../actions/snackbar";
 import {userWatchlistService} from "../../utils";
+import {MARKETDATA_ADDCOIN_TO_USERCOINS} from "../actions/marketdata";
 
 const state = {
     userWatchlist: [],
@@ -29,15 +35,54 @@ const actions = {
                 })
         })
     },
+    [WATCHLIST_ADD_NEW_COIN]: ({commit, dispatch, rootState}, {coinId, currency}) => {
+        return new Promise((resolve, reject) => {
+
+            return userWatchlistService.addNewWatchlistCoin(coinId, currency)
+                .then(resp => {
+
+                    // parsing response status to check if the coin was added successfully
+                    // or it is already in the watchlist
+                    let responseStatus = JSON.parse(resp);
+
+                    if (responseStatus.error_code === 400) {
+                        dispatch(SNACKBAR_ERROR, "The coin is already in your watchlist!");
+
+                    } else {
+
+                        dispatch(MARKETDATA_ADDCOIN_TO_USERCOINS, coinId)
+                            .then(() => {
+                                // get new coin's data to add to the user's watchlist store in the mutation
+                                let coinIdData = rootState.marketdata.allCoinsListData.find(coin => coin.id === coinId);
+
+                                commit(WATCHLIST_ADD_NEW_COIN, {coinIdData, currency});
+
+                                dispatch(SNACKBAR_SUCCESS, "The coin has been successfully added to your watchlist!");
+                                resolve(resp)
+                            })
+                            .catch(() => {
+                                dispatch(SNACKBAR_ERROR, "Error on adding a coin to watchlist!");
+                            });
+                    }
+
+                    resolve(resp);
+                })
+                .catch(err => {
+                    dispatch(SNACKBAR_ERROR, "Error on adding a coin to watchlist!");
+                    reject(err)
+                })
+        })
+    },
+
 };
 
 const mutations = {
     [WATCHLIST_SUCCESS]: (state, watchlist) => {
         state.userWatchlist = watchlist;
-        console.log('watchlist!!!', state.userWatchlist);
     },
     [WATCHLIST_CHANGE_COIN_CURRENCY]: (state, {coinId, currency}) => {
 
+        // set new currency to watchlist coin in store after set successfully response from backend
         state.userWatchlist.find(v => v.coinId.id === coinId).showedCurrency = currency;
 
         /* let ku = state.userWatchlist.find(function(e) {
@@ -46,55 +91,20 @@ const mutations = {
         ku.showedCurrency = currency;*/
 
     },
+    [WATCHLIST_ADD_NEW_COIN]: (state, {coinIdData, currency}) => {
+
+        // add to Vuex store "userWatchlist" a new element with data from allCoinsList and showed currency
+        state.userWatchlist.push({coinId: coinIdData, showedCurrency: currency});
+
+    },
     [WATCHLIST_ERROR]: (state, message) => {
         // state.userWatchlist = {};
     }
 };
+
 export default {
     state,
     getters,
     actions,
     mutations,
 }
-
-/*    [WATCHLIST_REQUESSSSSSSSSSST]: ({commit}, watchlist) => {
-        commit(WATCHLIST_SUCCESS, watchlist);
-
-        let timerId = setTimeout(function tick() {
-
-            console.log('tick');
-
-            userPortfolioService.getApplicationContainer()
-                .then(resp => {
-
-                    console.log(resp);
-/!*                    // parsing of response to have User entity as JSON
-                    let user = JSON.parse(resp);
-
-                    // save object Portfolio to separated 'store portfolio'
-                    dispatch(PORTFOLIO_SUCCESS, user.portfolio);
-
-                    // save array userWatchCoins to separated 'store watchlist'
-                    dispatch(WATCHLIST_SUCCESS, user.userWatchCoins);
-
-                    commit(USER_SUCCESS, user);
-
-                    // and also store new auth token to auth.store.sate
-                    commit(AUTH_SUCCESS);*!/
-
-                })
-                .catch(err => {
-                    // commit(USER_ERROR, err);
-                    // if resp is unauthorized, logout
-                    // dispatch(AUTH_LOGOUT)
-                });
-
-            timerId = setTimeout(tick, 60000);
-        }, 0);
-
-
-
-        /!*        setInterval(() => {
-            console.log('fuck intervaaaaaaaaaaaaaaaaal');
-        }, 5000)*!/
-    },*/
