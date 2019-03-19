ff48c4926e28bf5d73f9e554137148a871f7f9f9
lytves <22356770+lytves@users.noreply.github.com>
2019-03-19 21:15:28 +0100

    vuejs-mongodb: user.portfolio.service implemented response to "/portfolio-add-transaction" endpoint

--- crypfolio-vuejs-mongodb/frontend/src/utils/user.portfolio.service.js
+++ crypfolio-vuejs-mongodb/frontend/src/utils/user.portfolio.service.js
@@ -1,7 +1,8 @@
 import AXIOS from './axios-instance';
 
 export const userPortfolioService = {
-    setPortfolioShowedCurrency
+    setPortfolioShowedCurrency,
+    addTransaction
 };
 
 async function setPortfolioShowedCurrency(currency) {
@@ -15,6 +16,27 @@ async function setPortfolioShowedCurrency(currency) {
                 // we don't need to pass any data,
                 // coz it's a request only for set new portfolio currency
                 // if response was success, so setPortfolioShowedCurrency has been done successfully
+            })
+        .catch(error => {
+            return Promise.reject(error.response);
+        });
+}
+
+async function addTransaction(payload) {
+    return await AXIOS.post('/portfolio-add-transaction', {
+        "transCoinId": payload.transCoinId,
+        "transCurrency": payload.transCurrency,
+        "transType": payload.transType,
+        // always to convert numbers to strings
+        "transAmount": String(payload.transAmount),
+        "transPrice": String(payload.transPrice),
+        "transDate": payload.transDate,
+        "transComment": payload.transComment,
+    })
+        .then(
+            response => {
+
+                return response.data;
             })
         .catch(error => {
             return Promise.reject(error.response);
