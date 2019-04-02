1c82dd8a97aeb1f60c48876af7d3ee7184300e50
lytves <22356770+lytves@users.noreply.github.com>
2019-04-02 20:33:52 +0200

    vuejs-mongodb: added web/index.html

--- crypfolio-vuejs-mongodb/backend/src/main/java/tk/crypfolio/rest/SettingsRest.java
+++ crypfolio-vuejs-mongodb/backend/src/main/java/tk/crypfolio/rest/SettingsRest.java
@@ -3,7 +3,7 @@ package tk.crypfolio.rest;
 public class SettingsRest {
 
     public static final String ISSUER_INFO = "https://crypfolio.tk";
-    public static final String SUPER_SECRET_KEY = "Crypfolio.Tk.ndsjbf239bfjhbesfh";
+    public static final String SUPER_SECRET_KEY = "Crypfolio.Tk";
     public static final String TOKEN_BEARER_PREFIX = "Bearer";
     public static final int TOKEN_DURATION_DAYS = 30;
 }
