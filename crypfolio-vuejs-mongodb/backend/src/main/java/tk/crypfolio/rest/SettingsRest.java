a0d0a176c4de1376165ad43a3448bcbd6f4ea704
lytves <22356770+lytves@users.noreply.github.com>
2019-10-12 12:25:08 +0200

    vuejs-mongodb: removed https protocol en email links and REST setting for JWT token

--- crypfolio-vuejs-mongodb/backend/src/main/java/tk/crypfolio/rest/SettingsRest.java
+++ crypfolio-vuejs-mongodb/backend/src/main/java/tk/crypfolio/rest/SettingsRest.java
@@ -2,7 +2,7 @@ package tk.crypfolio.rest;
 
 public class SettingsRest {
 
-    public static final String ISSUER_INFO = "https://crypfolio.tk";
+    public static final String ISSUER_INFO = "http://crypfolio.tk";
     public static final String SUPER_SECRET_KEY = "Crypfolio.Tk";
     public static final String TOKEN_BEARER_PREFIX = "Bearer";
     public static final int TOKEN_DURATION_DAYS = 30;
